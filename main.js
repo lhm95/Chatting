db0b6e6732496e5365c17d34aaa401b33d2f9eda
H-web1 <hweb1011@gmail.com>
2020-03-29 01:27:23 +0900

    0329-2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $(function(){
+
         $('.chatWrap .chat .chatTitle .help p').click(function(){
           $('.chatWrap .faq').toggleClass('on');
         });
@@ -9,5 +10,21 @@ $(document).ready(function() {
         $('.chatWrap .chat .board .chatting .startBtn button').click(function(){
           $('.chatWrap .chat').addClass('on');
         });
+        $('.chatWrap .chat .chatTitle p.openBtn').click(function(){
+          $('.openTime').toggleClass('on');
+        });
+        $('.chatWrap .chat .chatTitle .basicBtn ul li .prev').click(function(){
+          $('.chatWrap .chat').removeClass('on');
+        });
+        $('.chatWrap .chat .chatTitle .basicBtn ul li .exit').click(function(){
+          $('#chatting').removeClass('on');
+          $('.chatWrap .chatBtn').addClass('on');
+        });
+        $('.chatWrap .chatBtn').click(function(){
+          $('#chatting').addClass('on');
+          $('.chatWrap .chatBtn').removeClass('on');
+        });
+
+
       });
 });
